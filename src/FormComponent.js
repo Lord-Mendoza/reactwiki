990a6d37deb6902cc752891c1e208c9e74fdfa8e
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-04-24 15:57:54 -0400

    Components
    Changes to Form and Mask Components

--- src/FormComponent.js
+++ src/FormComponent.js
@@ -9,11 +9,112 @@ class FormComponent extends Component{
     render(){
         return(
             <div>
-                <h2>Form Component</h2>
-                Where does it come from?
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                <div>
 
-                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                    <h1 className="display-4" style={{textAlign: "left", paddingLeft: 10}}>Form Component</h1>
+
+                    <h3 style={{textAlign: "left", paddingLeft: 50}}> Demonstration </h3>
+
+
+                    <h3 style={{textAlign: "left", paddingLeft: 50}}> Sample Usage </h3>
+
+                    <div className="highlight">
+                    <pre><code>&lt;FormComponent/&gt;;
+                    </code></pre>
+                    </div>
+
+                    <h3 style={{textAlign: "left", paddingLeft: 50}}> APIs </h3>
+                    <dl className = "row">
+                        <dt className="col-sm-3"> Name </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> configuration</dd>
+
+                        <dt className="col-sm-3"> Description </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}>
+                            <p>The list of form components to be generated where the label, type, placeholder and options (if applicable) are defined for each component.</p>
+                            <p>Notes:
+                                -Placeholder is an available option only to text-based form components. </p>
+                            <p>-When using the “select” type, the options property needs to be specified to render the available options of the select box.
+                                See example for sample usage. </p>
+                        </dd>
+
+                        <dt className="col-sm-3"> Values </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> An array of JSON objects</dd>
+
+                        <dt className="col-sm-3"> Example </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                    </dl>
+
+                    <hr width={"50%"}/>
+
+                    <dl className = "row">
+                        <dt className="col-sm-3"> Name </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> formValues</dd>
+
+                        <dt className="col-sm-3"> Description </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> This property uses the supplied callback function to pass the form values as typed by the user. </dd>
+
+                        <dt className="col-sm-3"> Values </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> A callback function </dd>
+
+                        <dt className="col-sm-3"> Example </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                    </dl>
+
+                    <hr width={"50%"}/>
+
+                    <dl className = "row">
+                        <dt className="col-sm-3"> Name </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> submitForm</dd>
+
+                        <dt className="col-sm-3"> Description </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}>
+                            <p>Toggles the FormComponent to submit the retrieved values from the user through the formValues callback function.</p>
+                            <p>Note: This will not clear the form.</p>
+                        </dd>
+
+                        <dt className="col-sm-3"> Values </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
+
+                        <dt className="col-sm-3"> Example </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                    </dl>
+
+                    <hr width={"50%"}/>
+
+                    <dl className = "row">
+                        <dt className="col-sm-3"> Name </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> clearForm</dd>
+
+                        <dt className="col-sm-3"> Description </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Toggles the FormComponent to clear the form. </dd>
+
+                        <dt className="col-sm-3"> Values </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
+
+                        <dt className="col-sm-3"> Example </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                    </dl>
+
+                    <hr width={"50%"}/>
+
+                    <dl className = "row">
+                        <dt className="col-sm-3"> Name </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> markInvalid</dd>
+
+                        <dt className="col-sm-3"> Description </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}>
+                            <p>This property is used by FormComponent to filter out the components that are invalid. Once marked invalid, the form component will have a red on its surrounding, highlighting to the user where they need to revise their entry.</p>
+                            <p>Note: FormComponent does not directly handle validating forms as to allow flexibility & prevent too much customized design. </p>
+                        </dd>
+
+                        <dt className="col-sm-3"> Values </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> An array </dd>
+
+                        <dt className="col-sm-3"> Example </dt>
+                        <dd className="col-sm-9" style={{textAlign: "left"}}> [ “Full name”, “Date of birth”] </dd>
+                    </dl>
+
+                </div>
             </div>
         );
     }
--- src/MaskComponent.js
+++ src/MaskComponent.js
@@ -9,14 +9,87 @@ class MaskComponent extends Component{
     render(){
         return(
             <div>
-                <h2>Mask Component</h2>
-                Where does it come from?
-                Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                <h1 className="display-4" style={{textAlign: "left", paddingLeft: 10}}>Mask Component</h1>
 
-                The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                <h3 style={{textAlign: "left", paddingLeft: 50}}> Demonstration </h3>
+
+
+                <h3 style={{textAlign: "left", paddingLeft: 50}}> Sample Usage </h3>
+
+                <div className="highlight">
+                    <pre><code>&lt;MaskComponent/&gt;;
+                    </code></pre>
+                </div>
+
+                <h3 style={{textAlign: "left", paddingLeft: 50}}> APIs </h3>
+                <dl className = "row">
+                    <dt className="col-sm-3"> Name </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> header </dd>
+
+                    <dt className="col-sm-3"> Description </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> The title of the popup. </dd>
+
+                    <dt className="col-sm-3"> Values </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> String </dd>
+
+                    <dt className="col-sm-3"> Example </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                </dl>
+
+                <hr width={"50%"}/>
+
+                <dl className = "row">
+                    <dt className="col-sm-3"> Name </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> content </dd>
+
+                    <dt className="col-sm-3"> Description </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> The message to appear in the mask popup. </dd>
+
+                    <dt className="col-sm-3"> Values </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> String </dd>
+
+                    <dt className="col-sm-3"> Example </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                </dl>
+
+                <hr width={"50%"}/>
+
+                <dl className = "row">
+                    <dt className="col-sm-3"> Name </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> show </dd>
+
+                    <dt className="col-sm-3"> Description </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}>
+                        <p>Toggles whether to show the mask or not.</p>
+                        <p>Note: It's ideal to assign a parent's state property here to toggle when to show a mask or not (which is ideal when performing a background process)</p>
+                    </dd>
+
+                    <dt className="col-sm-3"> Values </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
+
+                    <dt className="col-sm-3"> Example </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                </dl>
+
+                <hr width={"50%"}/>
+
+                <dl className = "row">
+                    <dt className="col-sm-3"> Name </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> loadingIcon </dd>
+
+                    <dt className="col-sm-3"> Description </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Toggles whether to show a rotating loading icon in the content. </dd>
+
+                    <dt className="col-sm-3"> Values </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
+
+                    <dt className="col-sm-3"> Example </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                </dl>
             </div>
         );
     }
 }
 
 export default MaskComponent;
+
