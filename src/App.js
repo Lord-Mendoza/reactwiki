bfeb34127bbc66a5d3d7ea8e13d28a794acc9d1d
Daniel Kim <daniel.kim2@caci.com>
2019-04-24 11:53:45 -0400

    Docs for React Components

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MainComponent from "./MainComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <h1> React Component Documentation </h1>
+        <MainComponent />
+    </div>
+  );
+}
+
+export default App;
