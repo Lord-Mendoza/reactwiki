7194a5ff14932818a495dbdc1d217d64c23fcee6
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-04-25 11:17:00 -0400

    Mask Component Completed

--- src/MaskComponent.js
+++ src/MaskComponent.js
@@ -1,9 +1,23 @@
 import React ,{Component} from 'react';
-import './MaskComponent.css';
+import './ComponentStyling.css';
+import {Image} from "react-bootstrap";
 
 class MaskComponent extends Component{
     constructor(props){
         super(props);
+    }
+
+    componentDidMount() {
+        this.runCodePrettify();
+    }
+
+    runCodePrettify() {
+        let script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+
+        script.src = 'https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
     }
 
     render(){
@@ -13,15 +27,41 @@ class MaskComponent extends Component{
 
                 <h3 style={{textAlign: "left", paddingLeft: 50}}> Demonstration </h3>
 
+                <Image
+                    src="/maskcomponent.gif"
+                    alt="Code for rendering form"
+                    style={{width: "50%"}}
+                />
 
-                <h3 style={{textAlign: "left", paddingLeft: 50}}> Sample Usage </h3>
+                {/*=======================================================
+                   ====================== Next Section ===================
+                   =======================================================*/}
+                <hr/>
+                <h3 style={{textAlign: "left", paddingLeft: 50}}> Sample Implementation </h3>
 
-                <div className="highlight">
-                    <pre><code>&lt;MaskComponent/&gt;;
-                    </code></pre>
-                </div>
+                <pre className="prettyprint lang-html">
+                    <code>
+                        <br/>
+                        <p> &lt;MaskComponent </p>
+                        <p>     //Required Ones </p>
+                        <p>     header=&#123;"Please Wait"&#125;</p>
+                        <p>     content=&#123;"Loading"&#125;</p>
+                        <p>     show=&#123;this.state.toggleShow&#125;</p>
+                        <br/>
 
+                        <p>     //Optional One </p>
+                        <p>     loadingIcon=&#123;true&#125; </p>
+                        /&gt;
+                    </code>
+                </pre>
+
+                {/*=======================================================
+                ====================== Next Section ======================
+                =======================================================*/}
+                <hr/>
                 <h3 style={{textAlign: "left", paddingLeft: 50}}> APIs </h3>
+
+                <h5 style={{textAlign: "left", paddingLeft: 50, color: "#63b4cf"}}> Required </h5>
                 <dl className = "row">
                     <dt className="col-sm-3"> Name </dt>
                     <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> header </dd>
@@ -31,12 +71,10 @@ class MaskComponent extends Component{
 
                     <dt className="col-sm-3"> Values </dt>
                     <dd className="col-sm-9" style={{textAlign: "left"}}> String </dd>
-
-                    <dt className="col-sm-3"> Example </dt>
-                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
                 </dl>
 
-                <hr width={"50%"}/>
+                {/*-----Next Entry-----*/}
+                <hr width={"85%"}/>
 
                 <dl className = "row">
                     <dt className="col-sm-3"> Name </dt>
@@ -47,31 +85,29 @@ class MaskComponent extends Component{
 
                     <dt className="col-sm-3"> Values </dt>
                     <dd className="col-sm-9" style={{textAlign: "left"}}> String </dd>
-
-                    <dt className="col-sm-3"> Example </dt>
-                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
                 </dl>
 
-                <hr width={"50%"}/>
+                {/*-----Next Entry-----*/}
+                <hr width={"85%"}/>
 
                 <dl className = "row">
                     <dt className="col-sm-3"> Name </dt>
                     <dd className="col-sm-9" style={{textAlign: "left", fontStyle: "italic"}}> show </dd>
 
                     <dt className="col-sm-3"> Description </dt>
-                    <dd className="col-sm-9" style={{textAlign: "left"}}>
-                        <p>Toggles whether to show the mask or not.</p>
-                        <p>Note: It's ideal to assign a parent's state property here to toggle when to show a mask or not (which is ideal when performing a background process)</p>
-                    </dd>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}>Toggles whether to show the mask or not.</dd>
 
                     <dt className="col-sm-3"> Values </dt>
                     <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
 
-                    <dt className="col-sm-3"> Example </dt>
-                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
+                    <dt className="col-sm-3"> Note </dt>
+                    <dd className="col-sm-9" style={{textAlign: "left"}}>It's ideal to assign a parent's state property here to toggle when to show a mask or not (which is ideal when performing a background process)</dd>
                 </dl>
 
-                <hr width={"50%"}/>
+                {/*====================== Additional Section ======================*/}
+
+                <hr width={"90%"}/>
+                <h5 style={{textAlign: "left", paddingLeft: 50, color: "#63b4cf"}}> Additional Customization </h5>
 
                 <dl className = "row">
                     <dt className="col-sm-3"> Name </dt>
@@ -82,9 +118,6 @@ class MaskComponent extends Component{
 
                     <dt className="col-sm-3"> Values </dt>
                     <dd className="col-sm-9" style={{textAlign: "left"}}> Boolean </dd>
-
-                    <dt className="col-sm-3"> Example </dt>
-                    <dd className="col-sm-9" style={{textAlign: "left"}}> Here's an example </dd>
                 </dl>
             </div>
         );
