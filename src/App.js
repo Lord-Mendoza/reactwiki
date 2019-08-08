df64ae8c956b6b6de040f2a8eca66cc546ee65f0
Lord Mendoza <lord.d.mendoza@gmail.com>
2019-08-08 08:36:12 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import MainComponent from "./MainComponent";
 
 function App() {
+  componentDidMount() {
+    document.title = 'React Wiki';
+  }
+  
   return (
     <div className="App">
         <MainComponent />
